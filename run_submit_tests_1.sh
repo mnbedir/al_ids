4ca872fa915851d93859cb0b56c5d0a121b30597
Münteha Nur Bedir Tüzün <muntehanurbedir@gmail.com>
2022-06-30 17:21:16 +0300

    Added agent experiment scripts

---
+++ run_submit_tests_1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ $# -ge 1 ]]; then
+	base_folder=$1
+	echo Submitting batch of tests using $base_folder
+else
+  echo "Provide base folder that contains test configurations"
+  exit
+fi
+
+
+for folder_path in $base_folder/*
+do
+	sbatch submit_test_arg_1.sh $folder_path
+done
+
